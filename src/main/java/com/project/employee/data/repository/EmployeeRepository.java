60b724b3909db9d5fa781f2a7fa281f4e515c78b
maxim_lupascu <maxim.lupascu@introduct.tech>
2023-07-26 19:48:11 +0300

    test

--- src/main/java/com/project/employee/data/repository/EmployeeRepository.java
+++ src/main/java/com/project/employee/data/repository/EmployeeRepository.java
@@ -1,7 +1,9 @@
 package com.project.employee.data.repository;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.project.employee.data.entity.Employee;
@@ -9,4 +11,7 @@ import com.project.employee.data.entity.Employee;
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     Employee findByUniqueCode(String uniqueCode);
     List<Employee> findByManager(Employee manager);
+
+    @Cacheable("employees")
+    Optional<Employee> findById(Long id);
 }
